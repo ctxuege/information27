90809dd5b71a28548bd8cfa236e3d789e474dd18
ctxuege <c921967783@qq.com>
2019-12-12 14:52:23 +0800

    抽取配置到单独配置文件

--- manage.py
+++ manage.py
@@ -6,33 +6,7 @@ from flask_wtf import CSRFProtect
 from flask_session import Session
 from flask_script import Manager
 from flask_migrate import Migrate,MigrateCommand
-
-
-class Config(object):
-    """工程配置信息"""
-    DEBUG = True
-
-    SECRET_KEY = "FiSZ+xxGvsGdVTJVMRtY8vp8OwYtD2pvHE6AyvI9kWnG+soU4zY00GS2zP4jH2oW"
-
-    # 为mysql 添加配置
-    SQLALCHEMY_DATABASE_URI = "mysql://root:root@47.98.142.158:3306/information27"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-    # redis 配置
-    REDIS_HOST = "115.238.87.197"
-    REDIS_PORT = 6381
-    REDIS_PASSWORD = 'yq2018'
-
-    # session配置
-    SESSION_TYPE = "redis"
-    # 是否开启session签名
-    SESSION_USE_SIGNER = True
-    # 指定session 保存到redis
-    SESSION_REDIS = StrictRedis(host=REDIS_HOST,port=REDIS_PORT,password=REDIS_PASSWORD)
-    # 设置session 过期
-    SESSION_PERMANENT = False
-    # 设置过期时间
-    permanent_session_lifetime = 86400 * 2
+from config import Config
 
 
 app = Flask(__name__)
@@ -54,10 +28,11 @@ Migrate(app,db)
 # 将迁移命令添加到manager
 manager.add_command('db',MigrateCommand)
 
+
 @app.route('/')
 def index():
     session["name"] = "itheima"
-    return 'index222'
+    return 'index'
 
 
 if __name__ == '__main__':
